6e8cd02d9b5048d87f4c44b9f694d52fc7294e58
Lukasz Matysik <lukasz.matysik6@gmail.com>
2019-06-13 19:31:13 +0200

    Error handling

--- app/src/main/java/com/swistak/bibleverseapp/FormActivity.kt
+++ app/src/main/java/com/swistak/bibleverseapp/FormActivity.kt
@@ -3,10 +3,13 @@ package com.swistak.bibleverseapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_form.*
 import android.text.SpannableStringBuilder
 import android.text.Editable
-
+import android.view.View
+import androidx.core.os.HandlerCompat.postDelayed
+import kotlinx.android.synthetic.main.activity_result.*
 
 
 class FormActivity : AppCompatActivity() {
@@ -24,10 +27,38 @@ class FormActivity : AppCompatActivity() {
 
 
         searchBtn.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            startActivity(intent)
+
+            when {
+                BeginVerseInput.text.isNullOrEmpty()-> displayErrorMsg("Enter begin verse range value!")
+                BookNameInput.text.isNullOrEmpty() -> displayErrorMsg("Enter book name!")
+                ChapterNumberInput.text.isNullOrEmpty() -> displayErrorMsg("Enter number of chapter!")
+                !EndVerseInput.text.isNullOrEmpty() && Integer.parseInt(EndVerseInput.text.toString()) < Integer.parseInt(BeginVerseInput.text.toString()) -> displayErrorMsg("End verse range value cannot be lower than begin range!")
+                else -> {
+                    val intent1 = Intent(this, ResultActivity::class.java)
+                    intent1.putExtra("bookName",BookNameInput.text.toString())
+                    intent1.putExtra("chapterNum",ChapterNumberInput.text.toString())
+                    intent1.putExtra("beginVerse",BeginVerseInput.text.toString())
+                    if(!EndVerseInput.text.isNullOrEmpty())
+                        intent1.putExtra("endVerse",ChapterNumberInput.text.toString())
+                    else
+                        intent1.putExtra("endVerse","0")
+                    intent1.putExtra("combinedText",CombinedTextSwitch.isChecked)
+                    startActivity(intent1)
+                }
+            }
         }
     }
 
+    private fun displayErrorMsg(message : String){
+        ErrorLabel.text = message
+        ErrorLabel.visibility = View.VISIBLE
+
+        Handler().postDelayed(
+            {
+                ErrorLabel.visibility = View.INVISIBLE
+            },
+            3000
+        )
+    }
 
 }
